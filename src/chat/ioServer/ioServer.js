a6acb3c2c021b49656eff5875db56ef4a1846058
juanzitof <juanzito.12@gmail.com>
2022-10-22 17:52:27 -0300

    first commit

---
+++ src/chat/ioServer/ioServer.js
@@ -0,0 +1,13 @@
+const MessagesService = require("../service/MessagesService");
+const service = new MessagesService (process.env.DATA_BASE_MESSAGES)
+
+const ioServer = async ( io, socket) => {
+    socket.emit("messages", await ServiceWorker.getAllMessages())
+    socket.on("new_message", async ( message ) => {
+        console.log("mensaje nuevo");
+        await service.addMessage(message)
+        io.sockets.emit("messages", await service.getAllMessages())
+      })
+}
+
+module.exports = ioServer
